5bbd27dab2d95481f5ad2724401e28f2244c8158
mike-goes-indie <kagiamichael6996@gmail.com>
2024-01-19 13:16:29 +0300

    fifth commit attempt 2 to fix the background images with hover effect

--- src/Components/LinkWithUnderlineHoverEffect.tsx
+++ src/Components/LinkWithUnderlineHoverEffect.tsx
@@ -9,7 +9,7 @@ interface linkWithUnderlineHoverProps {
 const LinkWithUnderlineHoverEffect = ({ link, styles }: linkWithUnderlineHoverProps): ReactElement => {
   return (
     <li  className={`${styles}  background-on-hover cursor-pointer w-fit`} > 
-       <a className='text-2xl  relative lg:text-3xl capitalize leading-[44px] lg:leading-[68px] tracking-[0.2px] text-white link font-bold  '>{link}</a>         
+       <a className='text-2xl  relative lg:text-3xl capitalize leading-[44px] lg:leading-[68px] tracking-[0.2px] text-white link after:bg-["/background-hover-images/underline.png"] font-bold  '>{link}</a>         
     </li>
   )
 }
